7b3fd8d628e9f041827bcace1f3fe20e579dd4d6
IvanPlaza <plazaivanalt@gmail.com>
2023-10-03 14:51:58 -0300

    Nuevo Proyecto

--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -16,19 +16,19 @@ export default function Projects () {
         <iframe className={styles.video} src="https://www.youtube.com/embed/ZgsCnI4u8uI?autoplay=1" title="Pi Foods Video" frameborder="0" allowfullscreen/>
       </div>
 
-      {/* <div className={styles.project}>
+      <div className={styles.project}>
         <div className={styles.container}>
-          <a href="https://pi-foods-ivan-plaza.vercel.app/" target="blank">
-            <img className={styles.img} align="center" src="https://img.freepik.com/vector-premium/plantilla-logotipo-pizza-adecuada-logotipo-restaurante-cafeteria_607277-267.jpg?w=2000" alt="https://discord.gg/KDFNA8Qf"/>
+          <a href="https://tres-en-raya-kappa.vercel.app/" target="blank">
+            <img className={styles.img} align="center" src="https://cdn-icons-png.flaticon.com/256/53/53654.png" alt="https://discord.gg/KDFNA8Qf"/>
           </a>
-          <a href="https://github.com/IvanPlaza44/PI-FOOD" target="blank">
+          <a href="https://github.com/IvanPlaza44/TresEnRaya" target="blank">
             <img className={styles.img} align="center" src="https://www.svgrepo.com/show/325236/github.svg" alt="https://github.com/IvanPlaza44/PI-FOOD"/>
           </a>
         </div>
-        <iframe className={styles.video} src="https://www.youtube.com/embed/ZgsCnI4u8uI?autoplay=1" title="Pi Foods Video" frameborder="0" allowfullscreen/>
+        <iframe className={styles.video} src="https://www.youtube.com/embed/l16CMeNXxec?autoplay=1" title="Pi Foods Video" frameborder="0" allowfullscreen/>
       </div>
 
-      <div className={styles.project}>
+      {/* <div className={styles.project}>
         <div className={styles.container}>
           <a href="https://pi-foods-ivan-plaza.vercel.app/" target="blank">
             <img className={styles.img} align="center" src="https://img.freepik.com/vector-premium/plantilla-logotipo-pizza-adecuada-logotipo-restaurante-cafeteria_607277-267.jpg?w=2000" alt="https://discord.gg/KDFNA8Qf"/>
